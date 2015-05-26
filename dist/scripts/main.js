c7a7d9e3b9ec0c4c996231aa6cdd695a643c5dd0
Joshua Lagrange <jlagrange87@gmail.com>
2015-05-25 21:40:42 -0500

    Normal

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,6 +1,4 @@
 $(document).on("ready", start);
-//↑↑↑ short hand for ↓↓↓↓
-// document.querySelectorAll(document).addEventListener("ready")
 
 function start(e){
 	var $inputBox = $("#todo-input");
@@ -8,7 +6,6 @@ function start(e){
 	var $todoForm = $("#todo-form");
 	var $list = $("#list")
 	var $clear = $("#clear");
-	var $li = $("li");
 	var list = [];
 	$todoForm.on("submit", addTodo);
 	$clear.on("click", clear);
@@ -21,7 +18,7 @@ function start(e){
 		}
 		else{
 		list.push($inputBox.val());
-		$inputBox.val("")
+		$inputBox.val("");
 		var listHtml = render(list);
 		$list.html(listHtml);
 		}
@@ -30,15 +27,20 @@ function start(e){
 	function render(todoList){
 		return "<ul><li>"+todoList.join("</li><li>")+"</li></ul>";
 	}
+	
 	function clear(e){
 	list = [];
 	$list.html("");
-}
+	}
 
 	function cross(e){
-	e.target.style.textDecoration = "line-through"
+	e.target.style.textDecoration = "line-through";
 	e.target.style.fontSize = "15px";
-}
+	}
 
 }
+
+
+
+
 
